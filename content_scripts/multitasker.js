44ae8c54eeee9895d7831e19ee330c5678e50b10
Mwale Kalenga <mwale.kalenga@prosites.space>
2022-12-03 15:24:01 +0200

    Enable comprehensive logging.

--- content_scripts/multitasker.js
+++ content_scripts/multitasker.js
@@ -1,74 +1,97 @@
-// Select the node that will be observed for mutations
-const targetNode = document.getElementsByClassName("sc-1g53n8-0")[0];
-let originalNumberOfTickets = document.querySelectorAll(
-  '.sc-1g53n8-0 > div[role="presentation"]'
-).length;
-console.log(originalNumberOfTickets);
-var mp3_url =
-  "https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3";
-var timer = null;
-let userRecentlyScrolled = false;
-
-function scrollStatus() {
-  if (userRecentlyScrolled) {
-    targetNode.style.backgroundColor = "aliceblue";
-    return true;
-  } else if (!userRecentlyScrolled) {
-    targetNode.style.backgroundColor = "yellowgreen";
-    return false;
+(() => {
+  if (window.hasRun) {
+    return;
   }
-}
+  window.hasRun = true;
 
-window.addEventListener(
-  "scroll",
-  function () {
-    if (timer !== null) {
-      clearTimeout(timer);
-    }
-    // targetNode.style.backgroundColor = "green";
-    userRecentlyScrolled = true;
-    console.log("User scrolling detected...");
-    scrollStatus();
-    timer = setTimeout(function () {
-      //   targetNode.style.backgroundColor = "yellow";
-      userRecentlyScrolled = false;
-      scrollStatus();
-      console.log("User has not scrolled in the recent past.");
-    }, 10000);
-  },
-  true
-);
+  setTimeout(() => console.log(age), 5000);
 
-// Options for the observer (which mutations to observe)
-const config = { attributes: false, childList: true, subtree: false };
-
-// Callback function to execute when mutations are observed
-const callback = (mutationList, observer) => {
-  let currentNumberOfTickets = document.querySelectorAll(
+  // Select the node that will be observed for mutations
+  const targetNode = document.getElementsByClassName("sc-1g53n8-0")[0];
+  let originalNumberOfTickets = document.querySelectorAll(
     '.sc-1g53n8-0 > div[role="presentation"]'
+  ).length;
+  console.log(originalNumberOfTickets);
+  var mp3_url =
+    "https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3";
+  var timer = null;
+  let userRecentlyScrolled = false;
+
+  function scrollStatus() {
+    if (userRecentlyScrolled) {
+      targetNode.style.backgroundColor = "aliceblue";
+      return true;
+    } else if (!userRecentlyScrolled) {
+      targetNode.style.backgroundColor = "yellowgreen";
+      return false;
+    }
+  }
+
+  window.addEventListener(
+    "scroll",
+    function () {
+      if (timer !== null) {
+        clearTimeout(timer);
+      }
+      // targetNode.style.backgroundColor = "green";
+      userRecentlyScrolled = true;
+      console.log("User scrolling detected...");
+      scrollStatus();
+      timer = setTimeout(function () {
+        //   targetNode.style.backgroundColor = "yellow";
+        userRecentlyScrolled = false;
+        scrollStatus();
+        console.log("User has not scrolled in the recent past.");
+      }, 10000);
+    },
+    true
   );
 
-  console.log(currentNumberOfTickets.length);
-  console.log("originalNumberOfTickets = " + originalNumberOfTickets);
-  if (currentNumberOfTickets.length > originalNumberOfTickets) {
-    console.log("The number of tickets has increased.");
-    if (scrollStatus() === false) {
-      new Audio(mp3_url).play();
-      alert("The number of tickets has increased.");
-    }
-  }
-  for (const mutation of mutationList) {
-    if (mutation.type === "childList") {
-      console.log("A child node has been added or removed.");
-    } else if (mutation.type === "attributes") {
-      console.log(`The ${mutation.attributeName} attribute was modified.`);
-    }
-  }
-  originalNumberOfTickets = currentNumberOfTickets.length;
-};
+  // Options for the observer (which mutations to observe)
+  const config = { attributes: false, childList: true, subtree: false };
 
-// Create an observer instance linked to the callback function
-const observer = new MutationObserver(callback);
+  // Callback function to execute when mutations are observed
+  const callback = (mutationList, observer) => {
+    let currentNumberOfTickets = document.querySelectorAll(
+      '.sc-1g53n8-0 > div[role="presentation"]'
+    );
+
+    console.log(currentNumberOfTickets.length);
+    console.log("originalNumberOfTickets = " + originalNumberOfTickets);
+    if (currentNumberOfTickets.length > originalNumberOfTickets) {
+      console.log("The number of tickets has increased.");
+      if (scrollStatus() === false) {
+        new Audio(mp3_url).play();
+        alert("The number of tickets has increased.");
+      }
+    }
+    for (const mutation of mutationList) {
+      if (mutation.type === "childList") {
+        console.log("A child node has been added or removed.");
+      } else if (mutation.type === "attributes") {
+        console.log(`The ${mutation.attributeName} attribute was modified.`);
+      }
+    }
+    originalNumberOfTickets = currentNumberOfTickets.length;
+  };
+
+  // Create an observer instance linked to the callback function
+  const observer = new MutationObserver(callback);
+
+  /**
+   * Listen for messages from the background script.
+   * Call "insertBeast()" or "removeExistingBeasts()".
+   */
+  browser.runtime.onMessage.addListener((message) => {
+    if (message.command === "start") {
+      console.log("Started observing...");
+      observer.observe(targetNode, config);
+    } else if (message.command === "stop") {
+      console.log("Stopped observing...");
+      observer.disconnect();
+    }
+  });
+})();
 
 // Start observing the target node for configured mutations
-observer.observe(targetNode, config);
+// observer.observe(targetNode, config);
--- popup/index.js
+++ popup/index.js
@@ -1,4 +1,72 @@
-console.log("Something random...");
+const age = 26;
+
+/**
+ * Listen for clicks on the buttons, and send the appropriate message to
+ * the content script in the page.
+ */
+function listenForClicks() {
+  document.addEventListener("click", (e) => {
+    console.log(`A click was logged: ${e.target.textContent}.`);
+    /**
+     * Given the name of a beast, get the URL to the corresponding image.
+     */
+    function standardiseInput(actionName) {
+      switch (actionName) {
+        case "Start":
+          return "start";
+        case "Stop":
+          return "stop";
+        case "Simulate":
+          return "simulate";
+        case "Reset":
+          return "reset";
+      }
+    }
+
+    /**
+     * Insert the page-hiding CSS into the active tab,
+     * then get the beast URL and
+     * send a "beastify" message to the content script in the active tab.
+     */
+    function observing(tabs) {
+      let command = standardiseInput(e.target.textContent);
+      browser.tabs.sendMessage(tabs[0].id, {
+        command,
+      });
+    }
+
+    /**
+     * Remove the page-hiding CSS from the active tab,
+     * send a "reset" message to the content script in the active tab.
+     */
+    function reset(tabs) {
+      observer.disconnect();
+    }
+
+    /**
+     * Just log the error to the console.
+     */
+    function reportError(error) {
+      console.error(`Could not beastify: ${error}`);
+    }
+
+    /**
+     * Get the active tab,
+     * then call "beastify()" or "reset()" as appropriate.
+     */
+    if (e.target.type === "reset") {
+      browser.tabs
+        .query({ active: true, currentWindow: true })
+        .then(reset)
+        .catch(reportError);
+    } else {
+      browser.tabs
+        .query({ active: true, currentWindow: true })
+        .then(observing)
+        .catch(reportError);
+    }
+  });
+}
 
 /**
  * There was an error executing the script.
@@ -17,5 +85,5 @@ function reportExecuteScriptError(error) {
  */
 browser.tabs
   .executeScript({ file: "/content_scripts/multitasker.js" })
-  .then()
+  .then(listenForClicks)
   .catch(reportExecuteScriptError);
